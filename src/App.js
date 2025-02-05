bdcd51ba257f3589cba6eb0f77244f67e6707001
sohee943 <sohee943lim@gmail.com>
2025-02-05 23:19:24 +0900

    startpage1

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Meet from './pages/meet.tsx'
 import './style/app.css';
 
 function App() {
-  // <Route path='/' element={<Start/>}></Route>
-  // <Route path='/Meet' element={<Meet />}></Route>
   const Page={
       start : <Start/>,
       meet: <Meet/>,
--- src/pages/start.tsx
+++ src/pages/start.tsx
@@ -1,3 +1,5 @@
+import title from '../assets/start/title.png'
+import startBtn from '../assets/start/start_btn.png'
 import React  from "react";
 import '../style/app.css';
 
@@ -5,12 +7,30 @@ function Start( ){
 
     return(
       <>
-        <div >
-          start
+        <div>
+          <img src={title} alt="" />
+          <img src={startBtn} alt="" />
         </div>
       </>
     )
 }
+
+
+/*
+
+const ImageWrap = styled.div`
+  margin: 0 auto
+`;
+
+export const Banner = () => {
+  return (
+    <ImageWrap>
+    </ImageWrap>
+  )
+};
+*/
+
+
 
 
 export default Start;
